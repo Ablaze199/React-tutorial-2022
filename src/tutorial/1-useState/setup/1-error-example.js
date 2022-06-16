a0d670e400c4ba99b602a316973ecb89322a648f
Ablaze99 <femiemma99@gmail.com>
2022-06-16 10:41:41 +0100

    react-2022

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -4,6 +4,7 @@ import React from 'react';
 const ErrorExample = () => {
   let title = 'random title';
   const handleClick = () =>{
+    title = 'hello people'
   console.log(title)
   }
   return (
