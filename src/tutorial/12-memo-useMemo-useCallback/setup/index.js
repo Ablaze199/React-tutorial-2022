ffab151e3a618ed05299fe16fcf1c4902eb575db
Ablaze99 <femiemma99@gmail.com>
2022-06-28 12:42:03 +0100

    react-2020

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -6,10 +6,19 @@ import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 const url = 'https://course-api.com/javascript-store-products'
 
 // every time props or state changes, component re-renders
-
+const calculateMostExpensive = (data) => {
+  return data.reduce((total,item) =>{
+    
+  })
+}
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
+const [cart, setCart] = useState(0);
+  const addToCart =useCallback(() => {
+    setCart(cart + 1);
+  },[])
+
 
   return (
     <>
@@ -17,22 +26,27 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{marginTop: '3rem'}}>cart : {cart}</h1>
+      <BigList products={products} addToCart={addToCart}/>
     </>
   )
 }
 
-const BigList = ({ products }) => {
+const BigList = React.memo (({ products, addToCart }) => {
+  useEffect(()=> {
+    console.log('big list called')
+  })
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product}
+        addToCart={addToCart}></SingleProduct>
       })}
     </section>
-  )
-}
+  );
+});
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
   let { name, price } = fields
   price = price / 100
   const image = fields.image[0].url
@@ -42,6 +56,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
   )
 }
